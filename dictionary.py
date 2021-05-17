ede414bb06e7e47dcb87899d53fec5cb7b360be3
Shavon Thadani <shavon.thadani@gmail.com>
2021-05-16 21:56:34 -0400

    V 1 of Dictionary script and json file that has definitions

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+import json
+import os
+from difflib import get_close_matches
+data = json.load(open("data.json"))
+
+def translate(w):
+    w = w.lower()
+    if w in data:
+        return data[w]
+    elif w.title() in data:
+        return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input("Did you mean {} instead? Enter Y if yes or N or no. ".format(get_close_matches(w, data.keys())[0]))
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word doesn't exist. Please double check it."
+        else:
+            return "we did not understand your entry"
+    else:
+        return "The word doesn't exist. Please double check it"
+
+word = input("enter word: ")
+
+output = translate(word)
+if type(output) == list:
+    for definition in output:
+        print(definition + "\n")
+else:
+    print(output)
